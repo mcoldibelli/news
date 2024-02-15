3e5e3956e8225a15b2fa1da42fc2b232369b04e0
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-15 18:08:35 -0300

    feat: add basic structure for static preview

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+function Home() {
+  return (
+    <main>
+      <img src="src/assets/ibge-logo.svg" alt="IBGE Logo" />
+      <header>IBGE News</header>
+      <div>
+        <img src="" alt="Thumbnail" />
+        <div>Notícia Destaque</div>
+      </div>
+      <div>
+        <ul>
+          <li>Mais recentes</li>
+          <li>Release</li>
+          <li>Notícia</li>
+          <li>Favoritas</li>
+        </ul>
+      </div>
+      <div>Grid de noticias</div>
+      <button>VEJA MAIS</button>
+    </main>
+  );
+}
+
+export default Home;
