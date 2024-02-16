4f816580a0b590e24713d2c59116f71c5fe4b675
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-15 21:10:03 -0300

    feat: calls for API data when App is mounted

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
 import Home from './pages/Home';
 import { GlobalStyles } from './styles/global';
+import fetchApi from './service/newsApi';
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = fetchApi();
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <GlobalStyles />
