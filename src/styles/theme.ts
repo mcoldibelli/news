6d85947a8cda5f70fda2522c082dce31ed9c0a5d
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-16 19:12:40 -0300

    style: add colors to pallette

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -8,8 +8,11 @@ export const theme = {
     primaryBackground: 'rgb(255,255,255)',
     secondaryBackground: 'rgb(233,233,233)',
     tertiaryBackground: 'rgb(76,76,76)',
+    quatertiaryBackground: 'rgb(50, 122, 173)',
+    quinaryBackground: 'rgb(38, 94, 131)',
 
     // box
     boxShadow: '0 0 10px rbga(0, 0, 0, 0.1)',
+    borderLine: '1px solid rgb(89, 89, 89)',
   },
 };
