0f72d5e7b332441fa745d77ad90e2807690eab7c
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-16 08:36:56 -0300

    feat: gets thumbnail of highlighted news

--- src/components/NewsHighlighted.tsx
+++ src/components/NewsHighlighted.tsx
@@ -1,9 +1,29 @@
+import useNews from '../hooks/useNews';
+import { IBGE_HOME } from '../utils/constants';
 import Thumbnail from './Thumbnail';
 
+const parseThumbnail = (thumbnail: string): string => {
+  if (thumbnail) {
+    const parsedThumbnail = JSON.parse(thumbnail);
+
+    return `${IBGE_HOME}${parsedThumbnail.image_intro}`;
+  }
+
+  return 'https://via.placeholder.com/300x200';
+};
+
 function NewsHighlighted() {
+  const { news } = useNews();
+  const highlighted = news[0];
+
+  console.log(highlighted);
+
   return (
     <section>
-      <Thumbnail src="https://via.placeholder.com/300" alt="Notícia Destaque" />
+      <Thumbnail
+        src={ parseThumbnail(highlighted.imagens) }
+        alt="Thumbnail da Notícia Destaque"
+      />
       <div>Notícia Destaque</div>
     </section>
   );
