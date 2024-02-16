f04d05551ae04f771485c6f16d19f1c5bff20f4e
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-16 09:41:31 -0300

    feat: add structure to news item

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -1,12 +1,29 @@
+import NewsCard from '../styles/newsCard';
+
 type NewsItemProps = {
+  id: number;
   title: string;
+  intro: string;
+  date: Date;
+  link: string;
 };
 
+function NewsItem({ id, title, intro, date, link }: NewsItemProps) {
 function NewsItem({ title }: NewsItemProps) {
+
   return (
-    <div>
+      <div>
+        <p>Notícia mais recente</p>
+        <span>Favorite button</span>
+      </div>
       <h2>{ title }</h2>
-    </div>
+      <p>{ intro }</p>
+      <div>
+        <button
+        >
+          Leia a notícia aqui
+        </button>
+      </div>
   );
 }
 
