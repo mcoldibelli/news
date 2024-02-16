399fb97c7e0fd521874bf37f25835da3cecb01d9
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-16 08:03:19 -0300

    refactor: give access to provider for all App components hierarchy

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { GlobalStyles } from './styles/global';
 
 function App() {
   return (
-    <NewsProvider>
+    <>
       <GlobalStyles />
       <Home />
-    </NewsProvider>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { NewsProvider } from './context/NewsProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+  <NewsProvider>
     <App />
+  </NewsProvider>,
   </React.StrictMode>,
 );
