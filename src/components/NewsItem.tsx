5d0fa3fde4062f8a89c519ebd0e9e67a5ed43845
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-16 14:41:23 -0300

    refactor: removes code that was placed on utils

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -2,27 +2,6 @@ import NewsCard from '../styles/newsCard';
 import daysSincePublished from '../utils/counter';
 import { NewsItemProps } from '../utils/types';
 
-type NewsItemProps = {
-  id: number;
-  title: string;
-  intro: string;
-  date: Date;
-  link: string;
-};
-
-const daysSincePublished = (date: Date): number => {
-  const now = new Date();
-  const differenceInMs = now.getTime() - date.getTime();
-
-  const differenceInDays = Math.floor(differenceInMs / (1000 * 60 * 60 * 24));
-
-  if (Number.isNaN(differenceInDays)) {
-    return 0;
-  }
-
-  return differenceInDays;
-};
-
 function NewsItem({ id, title, intro, date, link }: NewsItemProps) {
   const handleReadMoreClick = () => {
     window.open(link, '_blank');
