f0749474d612d398616565df23312faa787b4989
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-16 14:37:51 -0300

    feat: add news grid rendering half structured layout

--- src/components/NewsGrid.tsx
+++ src/components/NewsGrid.tsx
@@ -1,8 +1,33 @@
+import useNews from '../hooks/useNews';
+import NewsGridContainer from '../styles/newsGridContainer';
+import NewsItem from './NewsItem';
+
 function NewsGrid() {
+  const { news, isLoading } = useNews();
+
+  if (isLoading) {
+    return <p>Loading grid...</p>;
+  }
+
+  const newsList = news.splice(1, 9).map((item) => {
+    return (
+      <NewsItem
+        key={ item.id }
+        id={ item.id }
+        title={ item.title }
+        intro={ item.intro }
+        date={ item.date }
+        link=""
+      />
+    );
+  });
+
   return (
-    <section>
-      Grid de noticias
-    </section>
+    <NewsGridContainer>
+      <ul>
+        {newsList}
+      </ul>
+    </NewsGridContainer>
   );
 }
 
