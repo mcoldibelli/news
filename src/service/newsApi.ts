e68f2eb9d32b77f772af7c573f76419cafd31063
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-18 20:28:33 -0300

    refactor: improving fetch api

--- src/service/newsApi.ts
+++ src/service/newsApi.ts
@@ -1,9 +1,27 @@
 import { IBGE_ENDPOINT } from '../utils/constants';
 
-const fetchApi = async () => {
-  const response = await fetch(IBGE_ENDPOINT);
-  const data = await response.json();
-  return data.items;
+const fetchApi = async (page = 1, pageSize = 10) => {
+  const controller = new AbortController();
+  const timeoutId = setTimeout(() => controller.abort(), 5000);
+
+  try {
+    const response = await fetch(`${IBGE_ENDPOINT}/?qtd=${pageSize}&page=${page}`);
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.items;
+  } catch (err: any) {
+    if (err.name === 'AbortError') {
+      throw new Error('Fetch request timed out');
+    } else {
+      throw new Error('Failed to fetch news');
+    }
+  } finally {
+    clearTimeout(timeoutId);
+  }
 };
 
 export default fetchApi;
