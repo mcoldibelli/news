c0477ec882fc0e9f283abefcc089b0115ba85d92
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-18 20:31:06 -0300

    refactor: create a helper function aggregate

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,15 @@
 import { parse } from 'date-fns';
+import { IBGE_HOME } from './constants';
+
+export const parseThumbnail = (thumbnail: string): string => {
+  if (thumbnail) {
+    const parsedThumbnail = JSON.parse(thumbnail);
+
+    return `${IBGE_HOME}${parsedThumbnail.image_intro}`;
+  }
+
+  return 'https://via.placeholder.com/300x200';
+};
 
 export const daysSincePublished = (date: Date | undefined): number => {
   if (!date) return 0;
