e660cbb4d3388f8af1fb2b9f360221dc1bde7ffe
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-18 18:12:04 -0300

    refactor: change first fetch place

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useEffect } from 'react';
 import useNews from './hooks/useNews';
 import Home from './pages/Home';
 import { GlobalStyles } from './styles/global';
 
 function App() {
-  const { fetchNews, news, isLoading } = useNews();
-
-  useEffect(() => {
-    if (isLoading) {
-      fetchNews();
-    }
-  }, [isLoading]);
+  const { isLoading } = useNews();
 
   return (
     <>
