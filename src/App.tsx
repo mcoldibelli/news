c897402396d3ba2a71ab83a04bfde2d034ba13a6
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-19 17:38:53 -0300

    feat: add status while fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import useNews from './hooks/useNews';
+import { useContext } from 'react';
+import NewsContext from './context/NewsContext';
+import useFetchNews from './hooks/useFetchNews';
 import Home from './pages/Home';
 import { GlobalStyles } from './styles/global';
 
 function App() {
-  const { isLoading } = useNews();
+  useFetchNews();
+  const { fetchState } = useContext(NewsContext);
 
   return (
     <>
       <GlobalStyles />
-      {isLoading ? <p>Loading news...</p> : <Home />}
+      {fetchState.status === 'loading' && <p>Loading news...</p>}
+      {fetchState.status === 'success' && <Home />}
+      {fetchState.status === 'error' && (
+        <p>
+          Error loading news:
+          {fetchState.error.message}
+        </p>
+      )}
     </>
   );
 }
