2943867676b71f7f74b42e1fd74541cfb12025d6
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-19 21:18:35 -0300

    chore: adds strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App.tsx';
 import { NewsProvider } from './context/NewsProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <NewsProvider>
-      <Routes>
-        <Route path="/" element={ <App /> } />
-      </Routes>
-    </NewsProvider>
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <NewsProvider>
+        <Routes>
+          <Route path="/" element={ <App /> } />
+        </Routes>
+      </NewsProvider>
+    </BrowserRouter>
+    ,
+  </StrictMode>,
 );
