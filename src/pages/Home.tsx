54bda1feb75f8eb947d6634e63dac1a48f121bff
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-21 20:43:14 -0300

    style: apply stage on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,41 @@ import FilterNavigation from '../components/FilterNavigation';
 import Header from '../components/Header';
 import NewsGrid from '../components/NewsGrid';
 import NewsHighlighted from '../components/NewsHighlighted';
+import Pagination from '../components/Pagination';
 
-function Home() {
+/* eslint-disable react/jsx-max-depth */
+const posts = [
+  {
+    id: 1,
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      `Illo sint voluptas. Error voluptates culpa eligendi.
+      Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde.
+      Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.`,
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+  },
+];
+
+export default function Home() {
   return (
-    <>
-      <Header />
-      <FilterNavigation />
-      <main>
+    <div className="bg-white py-6 sm:py-10">
+      <div className="mx-auto max-w-7xl px-6 lg:px-12">
+        <Header />
+        <FilterNavigation />
         <NewsHighlighted />
-        <NewsGrid />
-      </main>
-    </>
+        <div
+          className="mx-auto mt-2 grid max-w-2xl
+          grid-cols-1 gap-x-8 gap-y-16 border-t
+          border-gray-200 pt-4 sm:mt-8 sm:pt-8
+          lg:mx-0 lg:max-w-none lg:grid-cols-3"
+        >
+          <NewsGrid />
+        </div>
+        <Pagination />
+      </div>
+    </div>
   );
 }
-
-export default Home;
