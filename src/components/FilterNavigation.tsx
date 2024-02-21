8ee69689c2848f1d63431876d320bbbacac6e862
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-21 20:45:08 -0300

    style: adds style to filter nav

--- src/components/FilterNavigation.tsx
+++ src/components/FilterNavigation.tsx
@@ -1,6 +1,5 @@
 import { IoMdSearch } from 'react-icons/io';
 import { useEffect, useState } from 'react';
-import Navigation from '../styles/filterNavigation';
 import useFilters from '../hooks/useFilters';
 
 function FilterNavigation() {
@@ -26,60 +25,88 @@ function FilterNavigation() {
   }, [setFilterType]);
 
   return (
-    <Navigation>
+    <div className="container mx-auto p-4 flex flex-col gap-4">
       <form
         action="/"
         method="get"
         aria-label="Search"
+        className="search-form flex items-center gap-2 bg-gray-100 p-2 rounded-md"
       >
-        <button aria-label="Search" className="search-icon" tabIndex={ 0 }>
+        <button
+          aria-label="Search"
+          tabIndex={ 0 }
+          className="search-icon p-2 bg-gray-200 rounded-md"
+        >
           <IoMdSearch />
         </button>
         <input
-          className="search-input"
           placeholder="Buscar"
           type="text"
           value={ searchText }
           onChange={ handleChange }
+          className="search-input flex-grow bg-transparent
+          focus:outline-none placeholder:text-gray-500"
         />
       </form>
 
-      <ul className="filter-buttons-container">
+      <ul
+        className="filter-buttons-container grid grid-cols-4 gap-2 text-center"
+      >
         <li>
           <button
             tabIndex={ 0 }
-            className={ getFilterClass('mostRecent') }
             onClick={ () => handleFilterClick('mostRecent') }
+            className={ `
+            ${getFilterClass('mostRecent')} 
+            bg-gray-200 text-gray-700 py-2 rounded-md
+            hover:bg-gray-300 
+            active:bg-blue-500 active:text-white 
+          ` }
           >
             Mais recentes
           </button>
         </li>
         <li>
           <button
-            className={ getFilterClass('release') }
             onClick={ () => handleFilterClick('release') }
+            className={ `
+            ${getFilterClass('release')} 
+            bg-gray-200 text-gray-700 py-2 rounded-md
+            hover:bg-gray-300 
+            active:bg-blue-500 active:text-white 
+          ` }
           >
             Release
           </button>
         </li>
         <li>
           <button
-            className={ getFilterClass('news') }
             onClick={ () => handleFilterClick('news') }
+            className={ `
+            ${getFilterClass('news')} 
+            bg-gray-200 text-gray-700 py-2 rounded-md
+            hover:bg-gray-300 
+            active:bg-blue-500 active:text-white 
+          ` }
           >
             Notícia
           </button>
         </li>
         <li>
           <button
-            className={ getFilterClass('favorites') }
             onClick={ () => handleFilterClick('favorites') }
+            className={ `
+            ${getFilterClass('favorites')} 
+            bg-gray-200 text-gray-700 py-2 rounded-md
+            hover:bg-gray-300 
+            active:bg-blue-500 active:text-white 
+          ` }
           >
             Favoritas
           </button>
         </li>
       </ul>
-    </Navigation>
+    </div>
   );
 }
 
