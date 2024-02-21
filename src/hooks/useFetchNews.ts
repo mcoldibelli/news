42f413a972576261938965f7f52e7fe2ac5df164
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-21 20:43:34 -0300

    feat: initial config for pagination

--- src/hooks/useFetchNews.ts
+++ src/hooks/useFetchNews.ts
@@ -1,8 +1,9 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import NewsContext from '../context/NewsContext';
 import fetchApi from '../service/newsApi';
 
 const useFetchNews = () => {
+  const [currentPage, setCurrentPage] = useState(1);
   const { fetchState, setFetchState } = useContext(NewsContext);
 
   useEffect(() => {
@@ -10,14 +11,17 @@ const useFetchNews = () => {
       setFetchState({ ...fetchState, status: 'loading' });
 
       try {
-        const newsData = await fetchApi();
+        const newsData = await fetchApi(currentPage);
         setFetchState({ ...fetchState, status: 'success', data: newsData });
       } catch (error) {
         setFetchState({ ...fetchState, status: 'error', error });
       }
     };
     fetchNewsData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  return { currentPage, setCurrentPage };
 };
 
 export default useFetchNews;
