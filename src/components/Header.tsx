71827786b62910d4ee665ade1918a9e821a55596
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-22 16:05:07 -0300

    style: change header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
 function Header() {
   return (
-    <div className="mx-auto max-w-2xl lg:mx-0">
-      <h1
-        className="text-5xl font-bold tracking-tight
-      text-gray-900 sm:text-6xl"
-      >
-        IBGE News
-      </h1>
-      <p className="mt-2 text-lg leading-8 text-gray-600">
-        Notícias sobre o IBGE em primeira mão.
-      </p>
+    <div className=" bg-gray-200 flex text-center">
+      <div className="max-w-2xl mx-auto px-4 lg:mx-0">
+        <h1
+          className="my-2 text-5xl font-bold tracking-tight
+           text-gray-900 sm:text-6xl"
+        >
+          IBGE News
+        </h1>
+        <p className="mt-2 text-md leading-8 text-gray-600">
+          Notícias sobre o IBGE em primeira mão.
+        </p>
+      </div>
     </div>
   );
 }
