bf63ef6ea9e06aa4ca015822eba041cc5e50afac
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-23 13:50:29 -0300

    refactor: removes dark theme

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,8 +1,7 @@
-import { IoMdSearch } from 'react-icons/io';
 import useFilters from '../hooks/useFilters';
 
 function Search() {
-  const { setFilterType, searchText, setSearchText } = useFilters();
+  const { setFilterType, setSearchText } = useFilters();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFilterType('search');
@@ -13,8 +12,7 @@ function Search() {
     <form className="max-w-md mx-auto px-2 mt-4 md:flex-grow">
       <label
         htmlFor="default-search"
-        className="mb-2 text-sm font-medium
-       text-gray-900 sr-only dark:text-white"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only"
       >
         Pesquisar
       </label>
@@ -25,17 +23,14 @@ function Search() {
           onChange={ handleChange }
           className="block w-full p-4 ps-10
          text-md text-gray-900 border border-gray-300 rounded-lg bg-gray-50
-          focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700
-           dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
-            dark:focus:ring-blue-500 dark:focus:border-blue-500 h-10"
+          focus:ring-blue-500 focus:border-blue-500 h-10"
           placeholder="Buscar"
         />
         <button
           type="submit"
           className="text-white absolute end-2.5 bottom-2.5 bg-blue-700
            hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300
-           font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600
-            dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+           font-medium rounded-lg text-sm px-4 py-2"
         >
           Buscar
         </button>
