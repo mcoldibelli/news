1bf550364a7e55c1afbb8d947c552658c2c24c33
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-23 16:00:31 -0300

    feat: fetches first page when user clicks the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,34 @@
-import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import NewsContext from '../context/NewsContext';
 
 function Header() {
+  const { fetchState, setFetchState } = useContext(NewsContext);
+
+  const handleRefresh = () => {
+    setFetchState({
+      ...fetchState,
+      pagination: {
+        ...fetchState.pagination,
+        page: 1 },
+    });
+  };
+
   return (
     <nav className="bg-gray-100 border-gray-200 shadow-lg">
       <div
         className="max-w-screen-xl flex flex-wrap items-center
           justify-between mx-auto p-4"
       >
-        <Link to="/">
-          <h1
-            className="text-4xl self-center font-semibold
+        <h1
+          className="text-4xl self-center font-semibold
             whitespace-nowrap"
+        >
+          <button
+            onClick={ handleRefresh }
           >
             IBGE News
-          </h1>
-        </Link>
+          </button>
+        </h1>
       </div>
     </nav>
   );
