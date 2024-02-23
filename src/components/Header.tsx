8deb73edc59637120980d2498469751cf6f0873f
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-23 13:47:04 -0300

    refactor: removes unused tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,18 +5,12 @@ function Header() {
         className="max-w-screen-xl flex flex-wrap items-center
           justify-between mx-auto p-4"
       >
-        <a
-          href="teste"
-          className="flex items-center space-x-3
-          rtl:space-x-reverse"
-        >
-          <span
-            className="text-4xl self-center font-semibold
+        <h1
+          className="text-4xl self-center font-semibold
             whitespace-nowrap"
-          >
-            IBGE News
-          </span>
-        </a>
+        >
+          IBGE News
+        </h1>
       </div>
     </nav>
   );
