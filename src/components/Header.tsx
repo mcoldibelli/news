389dbbc09fefc7539ba76d395baa885f2517472d
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-23 13:50:18 -0300

    feat: header redirects to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
     <nav className="bg-gray-100 border-gray-200 shadow-lg">
@@ -5,12 +7,14 @@ function Header() {
         className="max-w-screen-xl flex flex-wrap items-center
           justify-between mx-auto p-4"
       >
-        <h1
-          className="text-4xl self-center font-semibold
+        <Link to="/">
+          <h1
+            className="text-4xl self-center font-semibold
             whitespace-nowrap"
-        >
-          IBGE News
-        </h1>
+          >
+            IBGE News
+          </h1>
+        </Link>
       </div>
     </nav>
   );
