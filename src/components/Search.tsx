d83706401428cb11aa77308f3cc4138fd869bad6
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-24 12:34:33 -0300

    refactor: submit only after clicking on button

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,34 +3,37 @@ import useFilters from '../hooks/useFilters';
 function Search() {
   const { setFilterType, setSearchText } = useFilters();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     setFilterType('search');
-    setSearchText(e.target.value);
+    setSearchText(e.currentTarget.value);
   };
 
   return (
-    <form className="max-w-md mx-auto px-2 mt-4 md:flex-grow">
+    <form
+      onSubmit={ handleSubmit }
+      className="max-w-md mx-auto px-2 mt-4 md:flex-grow"
+    >
       <label
         htmlFor="default-search"
         className="mb-2 text-sm font-medium text-gray-900 sr-only"
       >
         Pesquisar
       </label>
-      <div className="relative">
+      <div className="relative flex-row-reverse">
         <input
           type="search"
           id="default-search"
-          onChange={ handleChange }
-          className="block w-full p-4 ps-10
+          className="w-auto p-4 ps-10
          text-md text-gray-900 border border-gray-300 rounded-lg bg-gray-50
           focus:ring-blue-500 focus:border-blue-500 h-10"
           placeholder="Buscar"
         />
         <button
           type="submit"
-          className="text-white absolute end-2.5 bottom-2.5 bg-blue-700
-           hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300
-           font-medium rounded-lg text-sm px-4 py-2"
+          className="justify-center items-center w-fit mx-2 h-10
+          px-4 py-2 text-sm font-medium text-white
+          bg-gray-800 hover:bg-gray-700 focus:outline-none
+          focus:bg-gray-700 rounded-md"
         >
           Buscar
         </button>
