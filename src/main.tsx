0038729614de6d7d3ff73c4385f72faa8e593905
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-02-24 13:18:03 -0300

    chore: removes react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,9 @@ import { NewsProvider } from './context/NewsProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <NewsProvider>
-        <Routes>
-          <Route path="/" element={ <App /> } />
-        </Routes>
-      </NewsProvider>
-    </BrowserRouter>
+    <NewsProvider>
+      <App />
+    </NewsProvider>
     ,
   </StrictMode>,
 );
