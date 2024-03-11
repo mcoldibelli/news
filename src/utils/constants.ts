30b55d470a815d423f8681da6f383c02b70d00c8
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-03-11 11:37:44 -0300

    refactor: change in fetch size

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const IBGE_ENDPOINT = 'https://servicodados.ibge.gov.br/api/v3/noticias';
 export const IBGE_HOME = 'https://www.ibge.gov.br/';
-export const NEWS_PER_PAGE = 9;
+export const NEWS_PER_PAGE = 10;
